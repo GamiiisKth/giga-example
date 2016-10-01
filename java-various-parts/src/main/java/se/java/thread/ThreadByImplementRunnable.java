bbe3ad7da614b10d7d84e505ad5cdcea95301d76
ali yusha <ali.yusha@so4it.com>
2016-10-01 22:34:00 +0200

    thread package has been added

--- java-various-parts/src/main/java/se/java/thread/ThreadByImplementRunnable.java
+++ java-various-parts/src/main/java/se/java/thread/ThreadByImplementRunnable.java
@@ -2,6 +2,23 @@ package se.java.thread;
 
 /**
  * @author Ali yusha {@literal <mailto:ali.yusha@so4it.com>}
+ *         http://www.studytonight.com/java/creating-a-thread.php
  */
-public class ThreadByImplementRunnable {
+public class ThreadByImplementRunnable implements Runnable {
+
+    @Override
+    public void run() {
+
+        System.out.println("concurrent thread started running..");
+
+
+    }
+
+
+    public static void main(String[] args) {
+        ThreadByImplementRunnable byImplementRunnable = new ThreadByImplementRunnable();
+
+        Thread thread = new Thread(byImplementRunnable);
+        thread.start();
+    }
 }
